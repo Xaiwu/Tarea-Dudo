ba77e507ff6635fc47f270431a7a348e7e1eec27
Jesús <neclxas3311@gmail.com>
2025-08-28 20:45:18 -0400

    GREEN: implementación mínima para [obtener la denominacione correspondiente al valor del dado]

--- src/juego/Dado.py
+++ src/juego/Dado.py
@@ -10,3 +10,14 @@ class Dado:
     def lanzar(self):
         self.valor = random.randint(1, 6)
         return self.valor
+    
+    def get_denominacion(self):
+        nombres = {
+            1: "As",
+            2: "Tonto",
+            3: "Tren",
+            4: "Cuadra",
+            5: "Quina",
+            6: "Sexto"
+        }
+        return nombres[self.valor]
