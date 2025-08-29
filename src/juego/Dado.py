5510af9d7b9f6bd071f9580794af1f927c16beaa
Jesús <neclxas3311@gmail.com>
2025-08-28 20:02:30 -0400

    REFACTOR: mejora código de [asignación de valor para un dado]

--- src/juego/Dado.py
+++ src/juego/Dado.py
@@ -1,8 +1,11 @@
 import random
 
 class Dado:
-    def __init__(self):
-        self.valor = random.randint(1, 6)
+    def __init__(self,valor=None):
+        if valor is not None:
+            self.valor = valor
+        else:
+            self.valor = random.randint(1, 6)
 
     def lanzar(self):
         self.valor = random.randint(1, 6)
