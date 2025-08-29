f9a30941052f2d85f8eedd7d1654c7c6552d072c
Jesús <neclxas3311@gmail.com>
2025-08-28 20:05:31 -0400

    RED: test para [obtención de denominaciones correspendiente al número del dado] - falla como esperado

--- tests/test_dado.py
+++ tests/test_dado.py
@@ -1,3 +1,4 @@
+import pytest
 from src.juego.Dado import Dado
 
 def test_lanzamiento_dado():
@@ -6,3 +7,9 @@ def test_lanzamiento_dado():
     valorDado = dado.lanzar()
 
     assert 1 <= valorDado <= 6
+
+@pytest.mark.parametrize("valor, esperado", [(1, "As"), (2, "Tonto"), (3, "Tren"), (4, "Cuadra"), (5, "Quina"), (6, "Sexto")])
+def test_denominacion(valor, esperado):
+    dado = Dado(valor)
+    resultado = dado.get_denominacion()
+    assert resultado == esperado
