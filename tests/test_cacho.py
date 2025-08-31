7f1ea61b4424c1eb343c0c4371b6de72c3a50cf1
Jesús <neclxas3311@gmail.com>
2025-08-30 20:31:40 -0400

    RED: test para [comprobar la visibilidad del cachosegún se requiera] - fallo como esperado

--- tests/test_cacho.py
+++ tests/test_cacho.py
@@ -7,3 +7,9 @@ def test_agitar_cacho():
     cacho.agitar()
     nuevosVal = cacho.get_valores()
     assert nuevosVal != valores
+
+def test_oculto_mostrar_cacho():
+    cacho = Cacho([1,2,3,4,5])
+    assert cacho.mostrar() == "X X X X X"
+    cacho.revelar()
+    assert cacho.mostrar() == "1 2 3 4 5"
