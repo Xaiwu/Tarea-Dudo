224237e48c42eb1e694b3213459411b03ea3b513
Jesús <neclxas3311@gmail.com>
2025-08-31 23:49:18 -0400

    REFACTOR: expando el test para comprobar la generación y administración de jugadores por parte del gestor de la partida

--- tests/test_gestor_partida.py
+++ tests/test_gestor_partida.py
@@ -1,7 +1,13 @@
 import pytest
 from src.juego.gestor_partida import GestorPartida
 
-@pytest.mark.parametrize("num_jugadores", [2, 3, 4])
-def test_gestor_partida_jugadores(num_jugadores):
+@pytest.mark.parametrize("num_jugadores,id_jugador", [(2, 1), (3, 2)])
+def test_gestor_partida_jugadores(num_jugadores,id_jugador):
     gestor = GestorPartida(num_jugadores)
     assert len(gestor.jugadores) == num_jugadores
+
+    gestor.jugadores[id_jugador].perder_dado()
+    assert len(gestor.jugadores[id_jugador].dados) == 4
+
+    gestor.jugadores[id_jugador].ganar_dado()
+    assert len(gestor.jugadores[id_jugador].dados) == 5
