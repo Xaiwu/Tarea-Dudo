89b74993a41fd88815a4fe4851deb36d2f6cf5db
Jesús <neclxas3311@gmail.com>
2025-09-01 18:34:52 -0400

    REFACTOR: mejora en implementación de lanzamiento de ddado, ahora usando mock

--- tests/test_dado.py
+++ tests/test_dado.py
@@ -1,12 +1,11 @@
 import pytest
 from src.juego.Dado import Dado
 
-def test_lanzamiento_dado():
+def test_lanzamiento_dado(mocker):
+    mocker.patch("random.randint", return_value=4)
     dado = Dado()
-
     valorDado = dado.lanzar()
-
-    assert 1 <= valorDado <= 6
+    assert valorDado == 4
 
 @pytest.mark.parametrize("valor, esperado", [(1, "As"), (2, "Tonto"), (3, "Tren"), (4, "Cuadra"), (5, "Quina"), (6, "Sexto")])
 def test_denominacion(valor, esperado):
