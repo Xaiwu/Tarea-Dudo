3c3eea835497cea26c7c4bf396bb1dc2d5503deb
Jesús <neclxas3311@gmail.com>
2025-08-31 23:06:24 -0400

    RED: test para [comprobar los jugadores generados] - falla como esperado

---
+++ tests/test_gestor_partida.py
@@ -0,0 +1,7 @@
+import pytest
+from src.juego.GestorPartida import GestorPartida
+
+@pytest.mark.parametrize("num_jugadores", [2, 3, 4])
+def test_gestor_partida_jugadores(num_jugadores):
+    gestor = GestorPartida(num_jugadores)
+    assert len(gestor.jugadores) == num_jugadores
