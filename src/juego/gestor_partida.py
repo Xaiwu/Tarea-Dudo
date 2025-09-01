7d53dd0db0e9fc8c07f1c12dee87410262ceed73
Jesús <neclxas3311@gmail.com>
2025-09-01 00:40:21 -0400

    GREEN: implementación para [determinar quien iniciará la partida]

--- src/juego/gestor_partida.py
+++ src/juego/gestor_partida.py
@@ -4,4 +4,13 @@ class GestorPartida:
     def __init__(self, num_jugadores):
         self.jugadores = [Cacho() for i in range(num_jugadores)]
 
-        
+
+    def determinar_iniciador(self):
+        while True:
+            valores = [jugador.get_valores()[0] for jugador in self.jugadores]
+            max_valor = max(valores)
+            candidatos = [jugador for jugador, valor in zip(self.jugadores, valores) if valor == max_valor]
+            if len(candidatos) == 1:
+                return candidatos[0]
+            for i in candidatos:
+                self.jugadores[i].dados[0].lanzar()
