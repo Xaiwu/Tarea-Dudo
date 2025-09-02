cf488f3924fcd3cef3cd00503a2f8d553ecc9e1f
Jesús <neclxas3311@gmail.com>
2025-09-02 14:02:17 -0400

    REFACTOR: Mejora de código para dado, mejor manejo de casos limite

--- src/juego/Dado.py
+++ src/juego/Dado.py
@@ -1,23 +1,26 @@
 import random
 
 class Dado:
+
     def __init__(self,valor=None):
         if valor is not None:
+            if valor < 1 or valor > 6:
+                raise ValueError("El valor del dado debe estar entre 1 y 6")
             self.valor = valor
         else:
             self.valor = random.randint(1, 6)
-
-    def lanzar(self):
-        self.valor = random.randint(1, 6)
-        return self.valor
-    
-    def get_denominacion(self):
-        nombres = {
+        self.NOMBRES = {
             1: "As",
             2: "Tonto",
             3: "Tren",
             4: "Cuadra",
             5: "Quina",
             6: "Sexto"
-        }
-        return nombres[self.valor]
+            }
+
+    def lanzar(self):
+        self.valor = random.randint(1, 6)
+        return self.valor
+    
+    def get_denominacion(self):
+        return self.NOMBRES[self.valor]
