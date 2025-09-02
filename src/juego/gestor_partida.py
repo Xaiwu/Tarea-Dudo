54d63f2a4d813c008fdc3065354e4532ca4f9a7c
Jesús <neclxas3311@gmail.com>
2025-09-01 20:10:45 -0400

    GREEN: implementación para [manejar el flujo correcto de los turnos]

--- src/juego/gestor_partida.py
+++ src/juego/gestor_partida.py
@@ -3,7 +3,7 @@ from src.juego.Cacho import Cacho
 class GestorPartida:
     def __init__(self, num_jugadores):
         self.jugadores = [Cacho() for i in range(num_jugadores)]
-
+        self.turno_actual = None
 
     def determinar_iniciador(self):
         while True:
@@ -11,6 +11,13 @@ class GestorPartida:
             max_valor = max(valores)
             candidatos = [jugador for jugador, valor in zip(self.jugadores, valores) if valor == max_valor]
             if len(candidatos) == 1:
+                self.turno_actual = self.jugadores.index(candidatos[0])
                 return candidatos[0]
             for cacho in candidatos:
                 cacho.dados[0].lanzar()
+
+    def jugador_actual(self):
+        return self.jugadores[self.turno_actual]
+
+    def siguiente_turno(self):
+        self.turno_actual = (self.turno_actual + 1) % len(self.jugadores)
